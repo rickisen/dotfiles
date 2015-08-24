203d681ef083436c13dfe787146aa398a724cf85
rickard.spam@gmail.com <rickisen@localhost.localdomain>
2015-08-24 23:04:08 +0200

    added support for new paths

--- linkItUp.sh
+++ linkItUp.sh
@@ -2,36 +2,37 @@
 # correct positions for the dotfiles, as well as adding the infamus dot to them
 
 # xmonad config
-ln -s xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+#ln -sf xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+
+mkdir ~/.i3 ~/.config ~/bin ~/bin/scripts ~/.vim
 
 # i3-gaps
-ln -s i3/config ~/.i3/config
-ln -s i3/i3status.cfg ~/.i3/i3status.cfg
+ln -sf i3/config ~/.i3/config
+ln -sf i3/i3status.cfg ~/.i3/i3status.cfg
 
 # compton
-ln -s compton/compton.conf ~/.compton.conf
+ln -sf compton/compton.conf ~/.compton.conf
 
 # conky
-ln -s conky/conkyleft.conkyrc ~/.conkyleft.conkyrc
-ln -s conky/conkyright.conkyrc ~/.conkyright.conkyrc
+ln -sf conky/conkyleft.conkyrc ~/.conkyleft.conkyrc
+ln -sf conky/conkyright.conkyrc ~/.conkyright.conkyrc
 
 # ranger
-ln -s ranger/rc.conf ~/.config/ranger/rc.conf
-ln -s ranger/rifle.conf ~/.config/ranger/rifle.conf
-ln -s ranger/scope.sh ~/.config/ranger/scope.sh
+ln -sf ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -sf ranger/scope.sh ~/.config/ranger/scope.sh
 
 # scripts
-mkdir ~/bin
-ln -s scripts/ip-info/InternetInfo.sh ~/bin/InternetInfo.sh
-ln -s scripts/ip-info/KillPing.sh ~/bin/KillPing.sh
-ln -s scripts/sonyLaptop/keyboard-backlight-toggle.sh ~/bin/keyboard-backlight-toggle.sh
-ln -s scripts/sonyLaptop/trackpad-toggle.sh ~/bin/trackpad-toggle.sh
+ln -sf scripts/ip-info/InternetInfo.sh ~/bin/InternetInfo.sh
+ln -sf scripts/ip-info/KillPing.sh ~/bin/KillPing.sh
+ln -sf scripts/sonyLaptop/keyboard-backlight-toggle.sh ~/bin/keyboard-backlight-toggle.sh
+ln -sf scripts/sonyLaptop/trackpad-toggle.sh ~/bin/trackpad-toggle.sh
 
 # vim
-ln -s vim/vimrc ~/.vimrc
-ln -s vim ~/.vim
+ln -sf vim/vimrc ~/.vimrc
+ln -sf vim ~/.vim
 
 # X
-ln -s X/xinitrc ~/.xinitrc
-ln -s X/Xresources ~/.Xresources
+ln -sf X/xinitrc ~/.xinitrc
+ln -sf X/Xresources ~/.Xresources
 
