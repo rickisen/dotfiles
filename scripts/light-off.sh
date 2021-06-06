203721b531e1f01b69ff9eb6f9e9604c88387107
rickisen <rickard.petersen@gmail.com>
2021-06-06 17:26:55 +0200

    Add light controll scripts

---
+++ scripts/light-off.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+curl -X PUT \
+	-d '{"on": false}' \
+	 "Philips-hue/api/2yTLtUp9kq5Jpfir-6rBDYU6EedG4Lm13-Ms6gYq/lights/6/state"
---
+++ scripts/light-on.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -X PUT \
+	-d '{"on": true, "bri": 128}' \
+	 "Philips-hue/api/2yTLtUp9kq5Jpfir-6rBDYU6EedG4Lm13-Ms6gYq/lights/6/state"
